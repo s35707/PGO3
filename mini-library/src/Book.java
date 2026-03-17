49440f9e5e2e8c47dc474cf341ff5db82ef72a04
s35707 <s35707@pjwstk.edu.pl>
2026-03-17 09:41:08 +0100

    Add main class

--- mini-library/src/Book.java
+++ mini-library/src/Book.java
@@ -11,14 +11,7 @@ public class Book {
             available = bookAvailable;
 
         }
-        public static void main(String[] args) {
-            Book book1 = new Book("Hamlet", "William Shakespeare", 104, true);
-            Book book2 = new Book("Pan Tadeusz", "Adam Mickiewicz", 365, true);
-            Book book3 = new Book("Dante's Inferno", "Dante Alighieri",320, true);
 
-
-
-        }
 
         public static void printInfo(Book book){
             System.out.println("Title: "+book.title);
---
+++ mini-library/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Hamlet", "William Shakespeare", 104, true);
+        Book book2 = new Book("Pan Tadeusz", "Adam Mickiewicz", 365, true);
+        Book book3 = new Book("Dante's Inferno", "Dante Alighieri",320, true);
+
+
+
+    }
+}
